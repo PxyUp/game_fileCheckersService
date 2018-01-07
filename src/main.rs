219f6a726263015b957f55d56400274f3fc6a874
Yura Panarin <tduble94@gmail.com>
2018-01-08 02:07:03 +0300

    connect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::Read;
+use std::io::Write;
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf;
+    loop {
+        buf = [0; 512];
+        let _ = match stream.read(&mut buf) {
+            Err(e) => panic!("Got an error: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    break;
+                }
+                m
+            }
+        };
+
+        match stream.write(&buf) {
+            Err(_) => break,
+            Ok(_) => continue,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:12315").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || handle_client(stream));
+            }
+
+            Err(e) => { 
+                println!("Connected failed")
+            }
+        }
+    }
 }
